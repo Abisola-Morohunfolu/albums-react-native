0bc37f8cc8bc803266b466b3221f12a66d0e2590
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2020-06-11 21:07:25 +0100

    added-header-component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header';
 
 const instructions = Platform.select({
 	ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -8,29 +9,30 @@ const instructions = Platform.select({
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.welcome}>Welcome to React Native!</Text>
-			<Text style={styles.instructions}>Are you feeling good tonught?</Text>
-			<Text style={styles.instructions}>{instructions}</Text>
-		</View>
+		// <View style={styles.container}>
+		<Header headerText="Albums" />
+		// {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
+		// <Text style={styles.instructions}>Are you feeling good tonught?</Text>
+		// <Text style={styles.instructions}>{instructions}</Text> */}
+		// </View>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
-});
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		justifyContent: 'center',
+// 		alignItems: 'center',
+// 		backgroundColor: '#fff',
+// 	},
+// 	welcome: {
+// 		fontSize: 20,
+// 		textAlign: 'center',
+// 		margin: 10,
+// 	},
+// 	instructions: {
+// 		textAlign: 'center',
+// 		color: '#333333',
+// 		marginBottom: 5,
+// 	},
+// });
